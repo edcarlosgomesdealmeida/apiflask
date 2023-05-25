e0513dea62343beff442c032209323b63306e5ee
Ed carlos <ed.carlosalmeidag@gmail.com>
2023-05-25 17:48:49 -0300

    Projeto api Flask

---
+++ server/instance.py
@@ -0,0 +1,34 @@
+from flask import Flask, Blueprint
+from flask_restplus import Api
+
+
+class Server():
+
+    def __init__(self, ):
+        self.app = Flask(__name__)
+        self.blueprint = Blueprint('api', __name__, url_prefix='/api')
+        self.api = Api(self.blueprint, doc='/doc', title='Sample Flask-SQLAlchemy')
+        self.app.register_blueprint(self.blueprint)
+
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+        self.app.config['PROPAGATE_EXCEPTIONS'] = True
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        self.book_ns = self.book_ns()
+
+    def book_ns(self, ):
+        return self.api.namespace(name='Books', description='boos related operations', path='/')
+
+    def run(self, ):
+        self.app.run(
+            port=5000,
+            debug=True,
+            host='localhost'
+            )
+
+
+server = Server()
+
+
+
+
